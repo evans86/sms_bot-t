437c7600e6419fbdd568ac93c9ade8313af3dc01
evans86 <evans86@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>
2023-03-16 00:02:20 +0300

    update api

--- app/Http/Controllers/Api/v1/OrderController.php
+++ app/Http/Controllers/Api/v1/OrderController.php
@@ -65,8 +65,8 @@ class OrderController extends Controller
             if (is_null($request->user_id))
                 return ApiHelpers::error('Not found params: user_id');
             $user = SmsUser::query()->where(['telegram_id' => $request->user_id])->first();
-            if (is_null($request->service))
-                return ApiHelpers::error('Not found params: service');
+//            if (is_null($request->service))
+//                return ApiHelpers::error('Not found params: service');
 //        if(is_null($request->user_secret_key))
 //            return ApiHelpers::error('Not found params: user_secret_key');
         if(is_null($request->public_key))
@@ -77,7 +77,7 @@ class OrderController extends Controller
 
             $country = SmsCountry::query()->where(['id' => $user->country_id])->first();
             $operator = SmsOperator::query()->where(['id' => $user->operator_id])->first();
-            $service = $request->service;
+            $service = $user->service;
 
             $result = $this->orderService->createOrder($service, $operator->title, $country->org_id, $user->id, $bot);
 
