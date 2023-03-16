0438f502548193f7b27caceba446c7f0e69b6471
evans86 <evans86@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>
2023-03-16 22:32:44 +0300

    add subtract balance

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,9 +15,8 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
-        \App\Http\Middleware\OwnCors::class,
         \App\Http\Middleware\TrustProxies::class,
-//        \Fruitcake\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
