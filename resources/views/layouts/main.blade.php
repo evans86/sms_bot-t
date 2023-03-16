056282234e72fa41f6bef3eb89a65927bcef2baa
evans86 <evans86@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>
2023-03-17 00:29:25 +0300

    update start page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,16 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ url('css/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css' )}}">
     <link rel="stylesheet" href="{{ asset('css/flag-icon.css' )}}"/>
     <title>Document</title>
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100" style="background-image: url('{{ asset('img/bg.svg')}}');">
 <div class="container">
     <div class="row">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route('activate.countries.index') }}">Страны</a>
+                <a class="navbar-brand" href="{{ route('welcome') }}">Главная</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
                         aria-label="Переключатель навигации">
@@ -22,6 +23,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
+                        <a class="nav-link" href="{{ route('activate.countries.index') }}">Список стран</a>
                         <a class="nav-link" href="{{ route('activate.product.index') }}">Список сервисов</a>
                         <a class="nav-link" href="{{ route('users.index') }}">Пользователи</a>
                         <a class="nav-link" href="{{ route('activate.order.index') }}">Заказы</a>
@@ -33,5 +35,14 @@
     </div>
     @yield('content')
 </div>
+
+<footer class="mt-auto">
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        Copyright © 2023 | Сreated by:
+        <a class="text-reset fw-bold" href="https://github.com/evans86">Rex</a>
+        | Powered By:
+        <a class="text-reset fw-bold" href="https://bot-t.com/">Bot-t.com</a>
+    </div>
+</footer>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Controller::class, 'welcome'])->name('welcome');
 
 /**
  * Роуты для админки (страны, операторы, сервисы)
