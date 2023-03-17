5389a8bf3af2cbde6da623c0a1e908f47e7f71e3
evans86 <evans86@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>
2023-03-17 22:12:10 +0300

    update start page

--- app/Http/Requests/Bot/BotUpdateRequest.php
+++ app/Http/Requests/Bot/BotUpdateRequest.php
@@ -16,14 +16,14 @@ class BotUpdateRequest extends FormRequest
      */
     public function rules()
     {
-//        return [
-//            'public_key' => 'required|string',
-//            'private_key' => 'required|string',
-//            'version' => 'required|string|min:1|max:1',
-//            'category_id' => 'required|integer|min:1',
-//            'percent' => 'required|integer|min:0',
-//            'api_key' => 'required|string',
-//        ];
+        return [
+            'public_key' => 'required|string',
+            'private_key' => 'required|string',
+            'version' => 'required|string|min:1|max:1',
+            'category_id' => 'required|integer|min:1',
+            'percent' => 'required|integer|min:0',
+            'api_key' => 'required|string',
+        ];
     }
 
     /**
