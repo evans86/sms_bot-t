552a7a2c4cc83654d66a1616e38923d59f594335
evans86 <evans86@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>
2023-03-17 22:43:03 +0300

    update start page

--- app/Services/Activate/OrderService.php
+++ app/Services/Activate/OrderService.php
@@ -38,6 +38,8 @@ class OrderService extends MainService
             $apiPrice = $smsActivate->getTopCountriesByService($service);
 
             $price = $apiPrice[$country]['retail_price'];
+            dd($price);
+
             $pricePercent = $price + ($price * ($bot->percent / 100));
 
             $result = [
