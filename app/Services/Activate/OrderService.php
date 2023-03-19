b5009d6ec5eed3069448058b154d385a0b8b72a2
evans86 <evans86@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>
2023-03-20 00:07:32 +0300

    global refactoring

--- app/Services/Activate/OrderService.php
+++ app/Services/Activate/OrderService.php
@@ -193,7 +193,7 @@ class OrderService extends MainService
         $link = 'https://api.bot-t.com/v1/module/user/subtract-balance';
         $public_key = '062d7c679ca22cf88b01b13c0b24b057'; //062d7c679ca22cf88b01b13c0b24b057
         $private_key = 'd75bee5e605d87bf6ebd432a2b25eb0e'; //d75bee5e605d87bf6ebd432a2b25eb0e
-        $user_id = '14347358'; //1028741753
+        $user_id = '398981226'; //1028741753
         $secret_key = 'b41c9f4e38d4954419a43b8b52d67575b41c9f4e52d6c011b41c6998bf81bf81';
         $amount = 1050; //1050
         $comment = 'Списание СМС';
