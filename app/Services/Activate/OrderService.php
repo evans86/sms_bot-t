fab41b977a849a79050982c41c6de5f377266599
evans86 <evans86@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>
2023-03-20 22:50:41 +0300

    global refactoring

--- app/Services/Activate/OrderService.php
+++ app/Services/Activate/OrderService.php
@@ -123,19 +123,23 @@ class OrderService extends MainService
 
         $serviceResults = $smsActivate->getActiveActivations();
 
-        $order->status = $this->getStatus($order->org_id, $bot);
-        $order->save();
+//        $order->status = $this->getStatus($order->org_id, $bot);
+//        $order->save();
 
-//        switch ($this->getStatus($order->org_id, $bot)) {
-//            case 5:
-//                $order->status = 3;
-//                $order->save();
-//                break;
-//            case 4:
-//                $order->status = 4;
-//                $order->save();
-//                break;
-//        }
+        switch ($this->getStatus($order->org_id, $bot)) {
+            case 5:
+                $order->status = 3;
+                $order->save();
+                break;
+            case 7:
+                $order->status = 7;
+                $order->save();
+                break;
+            case 4:
+                $order->status = 4;
+                $order->save();
+                break;
+        }
 
         if (time() >= $order->end_time) {
             switch ($order->status) {
